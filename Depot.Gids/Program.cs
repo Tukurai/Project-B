0819edcd8b9330d8ed1d20657145e08b7f7b763c
NeensHR <1072577@hr.nl>
2024-03-06 15:41:43 +0100

    Gids check tours

--- Depot.Gids/Program.cs
+++ Depot.Gids/Program.cs
@@ -1,23 +1,36 @@
 ﻿using Depot.Common.Navigation;
+using Depot.DAL;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.IO;
+using System.Text.Json;
+using Newtonsoft.Json;
 
 namespace Depot.Gids;
 
 class Program
 {
     private static Menu? consoleMenu;
+    private static DepotContext depotContext = new DepotContext();
 
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        depotContext.LoadJson();
+        Console.WriteLine("Hello, World! Please login to the Depot.");
 
-        consoleMenu = new Menu("Gids", "Maak uw keuze uit het menu hieronder:");
+        if (Login())
+        {
+            consoleMenu = new Menu("Gids", "Maak uw keuze uit de rondleidingen hieronder: ");
 
-        var afsluiten = new SubMenu('0', "Afsluiten", "Sluit het programma.", Close);
-        consoleMenu.AddMenuItem(afsluiten);
+            var afsluiten = new SubMenu('0', "Afsluiten", "Sluit het programma.", Close);
+            consoleMenu.AddMenuItem(afsluiten);
 
-        consoleMenu.Show();
+            var bekijken1 = new SubMenu('1', "Rondleidingen bekijken", "Rondleiding 1 bekijken", GetTour);
+            consoleMenu.AddMenuItem(bekijken1);
+
+            consoleMenu.Show();
+        }
     }
 
     private static void Close()
@@ -26,5 +39,49 @@ class Program
         {
             consoleMenu.IsShowing = false;
         }
+    }
+
+    private static bool Login()
+    {
+        Console.WriteLine("Enter your barcode to log in: ");
+        do
+        {
+            string enteredBarcode = Console.ReadLine() ?? "";
+
+            if (!int.TryParse(enteredBarcode, out int userId) || userId < 1)
+            {
+                if (!depotContext.Users.Where(u => u.Id == userId).Any())
+                {
+                    Console.SetCursorPosition(0, Console.CursorTop - 1);
+                    Console.WriteLine("Ongeldige invoer.");
+                }    
+            }
+        } while (true);
+
+        return true;
+    }
+
+    private static void GetTour(int tourNr)
+    {
+        var tour = depotContext.Tours.Where(t => t.Id == tourNr).FirstOrDefault();
+
+        Console.WriteLine($"Rondleiding {tour.Id}: {tour.Start}");
+        Console.WriteLine("Alle geregistreerde bezoekers voor deze rondleiding: ");
+        foreach (var ticketnummer in tour.Registrations)
+        {
+            Console.WriteLine($"Bezoeker {ticketnummer}");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Alle tickets in de wachtrij voor deze rondleiding: ");
+        foreach (var ticketnummer in tour.Queue)
+        {
+            Console.WriteLine($"Bezoeker {ticketnummer}");
+        }
+
+        // Andere opties toevoegen, verwijderen, inchecken
+        Console.WriteLine("Druk op enter om terug naar het hoofdmenu te gaan.");
+        consoleMenu?.Reset();
+        Console.ReadLine();
     }
 }
